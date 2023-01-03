bebce5dc109f5cd60a95b692f35109b8ebe925cb
Devon Carew <devoncarew@gmail.com>
2023-01-02 17:17:41 -0800

    add comments.wast

---
+++ test/spec/comments/comments_test.dart
@@ -0,0 +1,25 @@
+// Generated from spec/test/core/comments.wast.
+
+// ignore_for_file: non_constant_identifier_names, unused_local_variable
+
+import '../../src/infra.dart';
+import 'comments.0.dart' as comments_0;
+import 'comments.1.dart' as comments_1;
+import 'comments.2.dart' as comments_2;
+import 'comments.3.dart' as comments_3;
+
+void main() {
+  group('comments', () {
+    // module comments.0.dart (line 10)
+    var m0 = comments_0.Comments0Module();
+
+    // module comments.1.dart (line 56)
+    var m1 = comments_1.Comments1Module();
+
+    // module comments.2.dart (line 66)
+    var m2 = comments_2.Comments2Module();
+
+    // module comments.3.dart (line 75)
+    var m3 = comments_3.Comments3Module();
+  });
+}
